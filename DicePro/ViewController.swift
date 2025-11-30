250edba4494db68712375e69dfae6e034b8aa903
Dmitry Sokolov <126354494+dsokolovdev@users.noreply.github.com>
2025-11-30 21:46:12 +0400

    Initial Commit

---
+++ DicePro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicePro
+//
+//  Created by Dmitri  on 30.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
